7db48f8f846b5cd4b8b2eb34531fe923f1affaa8
AFloresc <afloresc@edu.tecnocampus.cat>
2018-05-25 14:20:12 +0200

    Working on Login and Signup APIs

--- src/main/java/com/inredec/ATutorBackend/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/inredec/ATutorBackend/security/JwtAuthenticationEntryPoint.java
@@ -3,6 +3,9 @@ package com.inredec.ATutorBackend.security;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,9 +13,15 @@ import java.io.IOException;
 
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint{
 
-    
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
+
     @Override
-    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+    public void commence(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
+        logger.error("Responding with unauthorized error. Message - {}", e.getMessage());
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                "Sorry, You're not authorized to access this resource.");
 
     }
 }
